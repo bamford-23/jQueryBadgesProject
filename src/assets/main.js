1dac36f87d3fcba1f83d5579828282f1dc1e200c
AlexBamford <alex.bamford@import.io>
2017-10-02 16:55:36 +0100

    comiting changes

--- src/assets/main.js
+++ src/assets/main.js
@@ -1,5 +1,40 @@
 $(function() {
+  $.ajax({
+    url: 'https://www.codeschool.com/users/alexbamfordimport.json',
+    dataType: 'jsonp',
+    success: function(response) {
+      addCourses(response.courses.completed);
+    }
+  });
 
-  // your code will go here
+  function addCourses(courses){
 
+    var $badges = $('#badges');
+
+    courses.forEach(function(course){
+
+      var $course = $('<div />', {
+        'class': 'course'
+      }).appendTo($badges);
+
+      var $title = $('<h3 />', {
+        text: course.title
+      }).appendTo($course);
+
+      var $image = $('<img />', {
+        src: course.badge
+      }).appendTo($course);
+
+      var $a = $('<a />', {
+        'class': 'btn btn-primary',
+        target: '_blank',
+        href: course.url,
+        text: "See Course"
+      }).appendTo($course);
+
+
+
+    })
+  courses.sort();
+  }
 });
